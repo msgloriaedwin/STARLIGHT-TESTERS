6edb90e95c80df69c8a8d2af9dd9d97e13a73586
Lucky Elliot <elliotlucky99@gmail.com>
2024-07-28 23:01:05 +0100

    Added_ui_folder

---
+++ app/components/testFile.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestFile = () => {
+  return (
+    <div>
+      All folders and components must be inside the UI folder
+    </div>
+  )
+}
+
+export default TestFile
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import TestFile from "./components/testFile";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Tean Bingo at work
+      <TestFile />
     </main>
   );
 }
