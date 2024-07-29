0bf25c0e66a68b42ff10d1839f1bcc74f7657b96
maiqel <michaelolowe321@gmail.com>
2024-07-29 22:35:17 +0100

    added components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import { ChooseAvatarField } from "./components/chooseAvatarField/chooseAvatarField";
 import ForgotPasswordForm from "./components/forms/forgotPasswordForm/forgotPassword";
+import OtpComponent from "./components/otp/otpcomponent";
 import Slider from "./components/slider";
 
 export default function Home() {
   return <main>
+    <div className="flex justify-center bg-[#f7EEE7] items-center py-10 px-2">
+      <OtpComponent email="amin***@gmail.com"/>
+    </div>
     <Slider mode="volume"/>
       <ForgotPasswordForm/>
+      {/* <ChooseAvatarField setAvatarUrl}/> */}
+
       
 
   </main>;
