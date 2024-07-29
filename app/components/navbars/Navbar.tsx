8ab625888ea1d9c6827cd2d7c74c8726449391cd
Lucky Elliot <elliotlucky99@gmail.com>
2024-07-29 23:36:07 +0100

    Navbar_for_card_selection

--- app/components/navbars/Navbar.tsx
+++ app/components/navbars/Navbar.tsx
@@ -10,10 +10,7 @@ type NavbarProp = {
 const Navbar = ({ className, children }: NavbarProp) => {
     return (
         <div className={`${className} flex items-center w-full bg-transparent md:bg-[#fffdfd] px-6 md:px-20 py-6`}>
-            <div>
                 {children}
-            </div>
-            
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import ForgotPasswordForm from "./components/forms/forgotPasswordForm/forgotPassword";
+import CardSelectionNavbar from "./components/navbars/custom-navbars/CardSelectionNavbar";
+import LandingPageNavbar from "./components/navbars/custom-navbars/LandingPageNavbar";
+import SignUpNavbar from "./components/navbars/custom-navbars/signUpNavbar";
 import Slider from "./components/slider";
 
 export default function Home() {
