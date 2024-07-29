28fb525cbf1b8980efa76289689b22add3e8bca6
Aminah Rashid <aminahrashid.bsaif20@iba-suk.edu.pk>
2024-07-29 17:50:59 +0500

    fix: add dummy resend click function in otp component

--- app/components/otp/otpcomponent.tsx
+++ app/components/otp/otpcomponent.tsx
@@ -9,23 +9,31 @@ interface OtpComponentProps {
 
 const OtpComponent: React.FC<OtpComponentProps> = ({ email }) => {
   const [otp, setOtp] = useState(Array(6).fill(""));
+  const [resendLoading, setResendLoading] = useState(false);
 
-  const handleOtpSubmit: () => void = () => {
+  const handleOtpSubmit = () => {
     const otpValue = otp.join("");
     console.log("OTP submitted:", otpValue);
   };
 
+  const handleResendOtp = () => {
+    console.log("Resend OTP clicked");
+  };
+
   return (
     <div className="bg-[#F7EEE7] w-fit text-[#00222E] text-center">
-      <h2 className="text-2xl xl:text-3xl mb-2 leading-[130%]">
-        Password Reset
-      </h2>
+      <h2 className="text-2xl xl:text-3xl mb-2 leading-[130%]">Password Reset</h2>
       <p className="mb-10 xl:text-lg text-sm">We have sent a code to {email}</p>
       <p className="mb-4 xl:text-lg text-sm">Enter password reset code here</p>
       <OtpInputComponent otp={otp} setOtp={setOtp} />
       <p className="mb-10 mt-4 xl:text-lg text-sm">
         <span>Didn’t receive a code?</span>{" "}
-        <span className="ml-1 text-[#00658B]">Resend</span>
+        <span
+          className={`ml-1 text-[#00658B] cursor-pointer ${resendLoading ? 'opacity-50' : ''}`}
+          onClick={!resendLoading ? handleResendOtp : undefined}
+        >
+          {resendLoading ? 'Resending...' : 'Resend'}
+        </span>
       </p>
       <Button
         onClick={handleOtpSubmit}
