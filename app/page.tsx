81ea990805c1e358e69d09f1e231771c865b0447
Lucky Elliot <elliotlucky99@gmail.com>
2024-07-31 00:04:59 +0100

    chores_fixed_navbar

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,37 @@ import React, { useState } from "react";
 import ForgotPasswordForm from "./components/forms/forgotPasswordForm/forgotPassword";
 import OtpComponent from "./components/otp/otpcomponent";
 import Slider from "./components/slider";
-import PasswordResetForm from "./components/passwordResetForm/passwordResetForm";
+import LandingPageNavbar from "./components/navbars/custom-navbars/LandingPageNavbar";
 
 export default function Home() {
   const onSubmit = (data: any) => {
     console.log(data, "data");
   };
+  const onLogin = () => {
+    //Login code here
+    console.log('clicked');
+  }
+  const onSignup = () => {
+    //Login code here
+    console.log('clicked');
+  }
+  const handleHowToPlayClick = () => {
+    //Logic to Display how to play content here
+    console.log('clicked');
+  }
+  const handleShowMenu = () => {
+    //Logic to Display leaderboard content here
+    console.log('clicked');
+  }
 
-  return (
-    <main>
-      <div className="flex justify-center bg-[#f7EEE7] items-center py-10 px-2">
-        <OtpComponent email="amin***@gmail.com" />
-      </div>
 
-      <Slider mode="volume" />
-      <ForgotPasswordForm />
-      <div className="flex justify-center py-10 px-2 ">
-        <PasswordResetForm link="/" onSubmit={onSubmit} />
-      </div>
-    </main>
-  );
+
+  return <main>
+    <LandingPageNavbar onLogin={onLogin} onSignup={onSignup} handleHowToPlayClick={handleHowToPlayClick} handleShowMenu={handleShowMenu} />
+    <div className="flex justify-center bg-[#f7EEE7] items-center py-10 px-2">
+      <OtpComponent email="amin***@gmail.com" />
+    </div>
+    <Slider mode="volume" />
+    <ForgotPasswordForm />
+  </main>;
 }
