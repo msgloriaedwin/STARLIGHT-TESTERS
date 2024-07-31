be98ceb420ff2dac48161fa49c024436fc9e53a2
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-07-31 05:04:52 +0100

    feat(15): Modified the active state and emoty card state for the game card component

--- app/components/game-card/GameCard.tsx
+++ app/components/game-card/GameCard.tsx
@@ -1,25 +1,49 @@
 "use client"
 import { useState } from 'react';
+import Image from "next/image";
+
+type OnCardActivateProps = {
+    value: string | number,
+    activeState: boolean
+}
 
 type GameCardProps = {
   value: string | number,
-  onCardActivate: (value: string | number) => void
+  onCardActivate?: ({value, activeState}: OnCardActivateProps) => void
 }
 
 const GameCard = ({value, onCardActivate}: GameCardProps) => {
   const [isCardActive, setIsCardActive] = useState(false);
 
-  const handleCardActive =()=>{
-    const newIsCardActive = !isCardActive
-    setIsCardActive(newIsCardActive)
-    console.log(newIsCardActive)
-    newIsCardActive ? onCardActivate( value ) : console.log(`Card ${value} is inactive`)
+  const handleCardActive = () => {
+    const activeState = true
+    setIsCardActive(true)
+    if (onCardActivate) {
+      onCardActivate({ value, activeState })
+    }
   }
 
   return (
-    <div onClick={() => handleCardActive()} className={`cursor-pointer md:max-w-14 max-w-10 w-full max-h-14 md:max-h-20 relative rounded-tl rounded-tr-none rounded-br rounded-bl-none  box-border overflow-hidden shrink-0 flex flex-col items-center justify-center py-4 md:py-[1.5rem] px-[1rem] text-center text-[1.8rem] md:text-[3rem] border-b-[3px] border-solid border-[#00a8e8] border-l-[3px] ${isCardActive ? "bg-[#00435d] text-[#00a8e8]" : "bg-[#ace8ff] text-[#00222e]"}`}>
-      <b className="relative leading-[140%]">{value}</b>
-    </div>
+        <>
+        {
+          value
+            ?
+            <div className='relative'>
+              <div onClick={() => !isCardActive && handleCardActive()} className={`z-1 relative cursor-pointer md:max-w-16 max-w-10 w-full max-h-14 md:max-h-20 rounded-tl rounded-tr-none rounded-br rounded-bl-none  box-border overflow-hidden shrink-0 flex flex-col items-center justify-center py-4 md:py-[1.5rem] px-[1rem] text-center text-[1.8rem] md:text-[2.7rem] border-b-[3px] border-solid border-[#00a8e8] border-l-[3px] text-[#00222e] ${isCardActive ? "bg-[#AEDCB0]" : "bg-[#ace8ff]"}`}>
+                <b className="relative leading-[140%]">{value}</b>
+              </div>
+              {
+                isCardActive 
+                ? <Image className="z-[99] absolute top-[-0.6rem] right-[-0.6rem] w-6 md:w-7" src="./tick-circle.svg" alt="green circle tick image" width="30" height="30"/> 
+                : ""
+              }
+            </div>
+          :
+          <div className={`cursor-pointer md:max-w-16 max-w-10 w-full max-h-14 md:max-h-20 relative rounded-tl rounded-tr-none rounded-br rounded-bl-none  box-border overflow-hidden shrink-0 flex flex-col items-center justify-center py-[1.5rem] md:py-[1.3rem] px-[0.4rem] md:px-[0.7rem] text-center text-[1.8rem] md:text-[3rem] border-b-[3px] border-solid border-[#00a8e8] border-l-[3px] bg-[#ace8ff] text-transaprent`}>
+            <Image src="./add-card.svg" alt="Add card Button" width={60} height={60}/>
+          </div>
+        }
+        </>
   )
 }
 
