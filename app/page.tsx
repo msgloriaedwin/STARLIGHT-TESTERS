ad88364e1be1e8d40d72b6a90854d9cc80283323
maiqel <michaelolowe321@gmail.com>
2024-07-31 19:02:45 +0100

    chore: added more components to homepage display

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,14 @@ import Slider from "./components/slider";
 import PasswordResetForm from "./components/passwordResetForm/passwordResetForm";
 import Footer from "./components/footer/footer";
 import GameOverModal from "./components/GameOverModal/GameOverModal";
-
+import CreateGameForm from "./components/forms/create-game-form/CreateGameForm";
+import { ToggleControl } from "./components/ToggleControl/ToggleControl";
 
 const players = [
-  { name: 'Precious', score: '3/5', imageUrl: '/assets/images/modalpic1.png' },
-  { name: 'Oluwole', score: '2/5', imageUrl: '/assets/images/modalpic3.png' },
-  { name: 'Sandra', score: '5/5', imageUrl: '/assets/images/modalpic4.png' },
-  { name: 'Farrell', score: '4/5', imageUrl: '/assets/images/modalpic2.png' },
+  { name: "Precious", score: "3/5", imageUrl: "/assets/images/modalpic1.png" },
+  { name: "Oluwole", score: "2/5", imageUrl: "/assets/images/modalpic3.png" },
+  { name: "Sandra", score: "5/5", imageUrl: "/assets/images/modalpic4.png" },
+  { name: "Farrell", score: "4/5", imageUrl: "/assets/images/modalpic2.png" },
 ];
 
 export default function Home() {
@@ -34,21 +35,34 @@ export default function Home() {
 
   return (
     <main>
-      <div className="flex justify-center bg-[#f7EEE7] items-center py-10 px-2">
-        <OtpComponent email="amin***@gmail.com" />
+      <div className='flex justify-center bg-[#f7EEE7] items-center py-10 px-2'>
+        <OtpComponent email='amin***@gmail.com' />
       </div>
 
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <button className="bg-blue-500 text-white py-2 px-4 rounded" onClick={handleOpenModal}>
-        Show Game Over Modal
-      </button>
-      <GameOverModal isOpen={isModalOpen} onClose={handleCloseModal} players={players} />
-    </div>
+      <div className='flex items-center justify-center min-h-screen bg-gray-100'>
+        <button
+          className='bg-blue-500 text-white py-2 px-4 rounded'
+          onClick={handleOpenModal}
+        >
+          Show Game Over Modal
+        </button>
+        <GameOverModal
+          isOpen={isModalOpen}
+          onClose={handleCloseModal}
+          players={players}
+        />
+      </div>
 
-      <Slider mode="volume" />
+      <div className='flex justify-center my-6'>
+        <CreateGameForm />
+      </div>
+      <div className='flex justify-center my-6'><ToggleControl /></div>
+
+      <Slider mode='volume' />
+      <Slider mode='level' className="mt-14" />
       <ForgotPasswordForm />
-      <div className="flex justify-center py-10 px-2 ">
-        <PasswordResetForm link="/" onSubmit={onSubmit} />
+      <div className='flex justify-center py-10 px-2 '>
+        <PasswordResetForm link='/' onSubmit={onSubmit} />
       </div>
       <Footer />
     </main>
