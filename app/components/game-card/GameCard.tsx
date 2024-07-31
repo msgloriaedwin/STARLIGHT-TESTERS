04029ea17e4ca846ea7203d249da3d27b4bb2673
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-07-31 08:29:31 +0100

    feat(15): Update made the value props optional and updated the styling to follow the custom properties

--- app/components/game-card/GameCard.tsx
+++ app/components/game-card/GameCard.tsx
@@ -8,7 +8,7 @@ type OnCardActivateProps = {
 }
 
 type GameCardProps = {
-  value: string | number,
+  value?: string | number,
   onCardActivate?: ({value, activeState}: OnCardActivateProps) => void
 }
 
@@ -18,7 +18,7 @@ const GameCard = ({value, onCardActivate}: GameCardProps) => {
   const handleCardActive = () => {
     const activeState = true
     setIsCardActive(true)
-    if (onCardActivate) {
+    if (onCardActivate && value) {
       onCardActivate({ value, activeState })
     }
   }
@@ -29,7 +29,7 @@ const GameCard = ({value, onCardActivate}: GameCardProps) => {
           value
             ?
             <div className='relative'>
-              <div onClick={() => !isCardActive && handleCardActive()} className={`z-1 relative cursor-pointer md:max-w-16 max-w-10 w-full max-h-14 md:max-h-20 rounded-tl rounded-tr-none rounded-br rounded-bl-none  box-border overflow-hidden shrink-0 flex flex-col items-center justify-center py-4 md:py-[1.5rem] px-[1rem] text-center text-[1.8rem] md:text-[2.7rem] border-b-[3px] border-solid border-[#00a8e8] border-l-[3px] text-[#00222e] ${isCardActive ? "bg-[#AEDCB0]" : "bg-[#ace8ff]"}`}>
+              <div onClick={() => !isCardActive && handleCardActive()} className={`z-1 relative cursor-pointer md:max-w-16 max-w-10 w-full max-h-14 md:max-h-20 rounded-tl rounded-tr-none rounded-br rounded-bl-none  box-border overflow-hidden shrink-0 flex flex-col items-center justify-center py-4 md:py-[1.5rem] px-[1rem] text-center text-[1.8rem] md:text-[2.7rem] border-b-[3px] border-solid border-primaryBlue border-l-[3px] text-[#00222e] ${isCardActive ? "bg-green-dirty-green" : "bg-form-blue"}`}>
                 <b className="relative leading-[140%]">{value}</b>
               </div>
               {
@@ -39,7 +39,7 @@ const GameCard = ({value, onCardActivate}: GameCardProps) => {
               }
             </div>
           :
-          <div className={`cursor-pointer md:max-w-16 max-w-10 w-full max-h-14 md:max-h-20 relative rounded-tl rounded-tr-none rounded-br rounded-bl-none  box-border overflow-hidden shrink-0 flex flex-col items-center justify-center py-[1.5rem] md:py-[1.3rem] px-[0.4rem] md:px-[0.7rem] text-center text-[1.8rem] md:text-[3rem] border-b-[3px] border-solid border-[#00a8e8] border-l-[3px] bg-[#ace8ff] text-transaprent`}>
+          <div className={` cursor-pointer md:max-w-16 max-w-10 w-full max-h-14 md:max-h-20 relative rounded-tl rounded-tr-none rounded-br rounded-bl-none  box-border overflow-hidden shrink-0 flex flex-col items-center justify-center py-[1.5rem] md:py-[1.3rem] px-[0.4rem] md:px-[0.7rem] text-center text-[1.8rem] md:text-[3rem] border-b-[3px] border-solid border-primaryBlue border-l-[3px] bg-form-blue text-transaprent`}>
             <Image src="./add-card.svg" alt="Add card Button" width={60} height={60}/>
           </div>
         }
