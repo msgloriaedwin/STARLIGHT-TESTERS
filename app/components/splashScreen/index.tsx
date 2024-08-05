a034b6763f0e0019008d3fb6d3722e0e54cefe53
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-08-06 00:27:54 +0100

    update splash Image and animation

--- app/components/splashScreen/index.tsx
+++ app/components/splashScreen/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import logo from "@/public/assets/images/Remote Bingo Logo.svg";
+import logo from "../../../public/assets/images/Remote Bingo Logo A.png";
 
 const SplashScreen = () => {
   const [showSplash, setShowSplash] = useState(true);
@@ -18,18 +18,13 @@ const SplashScreen = () => {
     return null;
   }
   return (
-    <div className="bg-body flex flex-col items-center justify-center w-screen h-screen gap-6">
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-body gap-6">
       <Image
-        className="w-full h-auto max-w-[242.678px] max-h-[99.85px] md:max-w-[321.49px] md:max-h-[132.277px] flex-shrink-0 animate-bounce"
+        className="w-full h-auto max-w-[242.678px] max-h-[99.85px] md:max-w-[321.49px] md:max-h-[132.277px]"
         src={logo}
         alt="Remote Bingo"
       />
-      <p
-        className="text-center font-dm-sans font-bold text-primary-900 custom-font-features
-        text-[27.175px] leading-[34.723px] 
-        md:text-[36px] md:leading-[46px] 
-      "
-      >
+      <p className="text-center font-bold text-primary-900 font-dm-sans custom-font-features text-[27.175px] leading-[34.723px] md:text-[36px] md:leading-[46px]">
         Welcome to Remote Bingo
       </p>
     </div>
