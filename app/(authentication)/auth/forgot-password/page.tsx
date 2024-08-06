d0471cbaa132be868211a8cbb2c991e99416422d
maiqel <michaelolowe321@gmail.com>
2024-08-06 06:03:02 +0100

    feat: implemented complete forgot password flow

---
+++ app/(authentication)/auth/forgot-password/page.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import CustomButton from '@/app/components/button/custombutton'
+import FormCard from '@/app/components/formcard/formCard'
+import RBInput from '@/app/components/input'
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation';
+import Link from 'next/link'
+import ForgotPasswordNavbar from "@/app/components/navbars/custom-navbars/ForgotPasswordNavbar";
+
+
+const ForgotPassword = () => {
+
+    const [email, setEmail] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    localStorage.setItem('resetEmail', email);
+    router.push('/auth/forgot-password/otp');
+  };
+  return (
+    <>
+    <ForgotPasswordNavbar/>
+    <div className="bg-body flex flex-col h-screen justify-center items-center">
+        <div className="mb-6">
+            <h3 className='text-2xl md:text-4xl mb-2'>Forgot Password?</h3>
+            <p className='text-sm md:text-lg'>Enter your Email to receive a code</p>
+        </div>
+        <FormCard>
+        <form onSubmit={handleSubmit} className='space-y-4'>
+            <RBInput
+              label='Enter Email'
+              placeholder='Your Email'
+              type="email" 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+            />
+            <CustomButton size={"lg"} className='w-full p-6' type='submit'>
+              Reset password
+            </CustomButton>
+          </form>
+            <Link href={'/auth/login'} className='underline flex justify-center my-4'>Back to sign in</Link>
+        </FormCard>
+    </div>
+    </>
+  )
+}
+
+export default ForgotPassword
