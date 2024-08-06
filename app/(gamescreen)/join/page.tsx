75324ccee3e0c86fb49eb3c9f81b6cdfdd9db83e
Adekola Toniloba <atoniloba@gmail.com>
2024-08-06 13:34:33 +0100

    cleared merge conflict

--- app/(gamescreen)/join/page.tsx
+++ app/(gamescreen)/join/page.tsx
@@ -1,13 +1,13 @@
 import JoinGameForm from "@/app/components/forms/join-game-form";
-import avatar1 from "@/assets/images/avatar-1.png";
-import avatar2 from "@/assets/images/avatar-2.png";
-import avatar3 from "@/assets/images/avatar-3.png";
-import avatar4 from "@/assets/images/avatar-4.png";
-import avatar5 from "@/assets/images/avatar-5.png";
-import avatar6 from "@/assets/images/avatar-6.png";
-import avatar7 from "@/assets/images/avatar-7.png";
-import avatar8 from "@/assets/images/avatar-8.png";
-import avatar9 from "@/assets/images/avatar-9.png";
+import avatar1 from "../../../public/assets/images/avatar-1.png";
+import avatar2 from "../../../public/assets/images/avatar-2.png";
+import avatar3 from "../../../public/assets/images/avatar-3.png";
+import avatar4 from "../../../public/assets/images/avatar-4.png";
+import avatar5 from "../../../public/assets/images/avatar-5.png";
+import avatar6 from "../../../public/assets/images/avatar-6.png";
+import avatar7 from "../../../public/assets/images/avatar-7.png";
+import avatar8 from "../../../public/assets/images/avatar-8.png";
+import avatar9 from "../../../public/assets/images/avatar-9.png";
 
 const Page = () => {
   const avatars = [
