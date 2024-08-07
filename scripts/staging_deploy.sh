9ee71aed72f11644a31b19cd6da66ff361be1cfd
Tella Boluwatife <42048129+codeReaper0@users.noreply.github.com>
2024-08-07 18:34:47 +0100

    Update staging_deploy.sh

--- scripts/staging_deploy.sh
+++ scripts/staging_deploy.sh
@@ -4,5 +4,6 @@ set -e
 
 cd "$(git rev-parse --show-toplevel)"
 git stash
-git pull origin dev
+git rebase
+git pull origin staging
 docker compose --project-name staging-bingofe -f docker/staging/docker-compose.yml up -d
