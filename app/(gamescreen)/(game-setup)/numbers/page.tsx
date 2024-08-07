9557fb77216e1f038c5be5018994b8dffb572eab
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-08-07 08:19:39 +0100

    feat: Developed the Game Number Selection page for the waitingr room of the bingo game

---
+++ app/(gamescreen)/(game-setup)/numbers/page.tsx
@@ -0,0 +1,12 @@
+import GameCardSelectionLobby from "@/app/components/gamecardselection/GameCardSelectionLobbyNumber"
+import SelectedGameCardRoom from "@/app/components/gamecardselection/SelectedGameCardRoom"
+// import GameCardSelection from "@/app/components/gamecardselection/GameCardSelectionRoom"
+
+const NumberPage = () => {
+	return (
+		<div className="relative ">
+			<GameCardSelectionLobby />
+		</div>
+	)
+}
+export default NumberPage
---
+++ data/data.ts
@@ -0,0 +1,3 @@
+import { generateUniqueRandomNumbers } from "@/lib/customFunc"
+
+export const initialNumbers = generateUniqueRandomNumbers(36, 1, 99)
