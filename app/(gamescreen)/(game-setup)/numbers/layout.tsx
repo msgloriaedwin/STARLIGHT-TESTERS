f5dc5a872f4c7e7a2a484a7ed0c8b6fdfe20ae98
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-08-08 09:57:22 +0100

    feat: Added a new standalone page for the number layout

---
+++ app/(gamescreen)/(game-setup)/numbers/layout.tsx
@@ -0,0 +1,47 @@
+// export default function RootLayout({
+// 	children,
+// }: Readonly<{
+// 	children: React.ReactNode
+// }>) {
+// 	return (
+// 		<html lang="en">
+// 			<body className={`bg-body`}>{children}</body>
+// 		</html>
+// 	)
+// }
+
+"use client"
+import { useBackgroundSound } from "@/utils/game-sounds/useBackgroundMusic"
+import { useEffect } from "react"
+import { usePathname } from "next/navigation"
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	const pathname = usePathname()
+	const { playSound, stopSound } = useBackgroundSound({
+		soundSrc: "/assets/audios/game-music.mp3",
+		enabled: true,
+	})
+
+	useEffect(() => {
+		if (pathname === "/create-game" || pathname === "/join") {
+			playSound()
+		} else {
+			stopSound()
+		}
+
+		return () => {
+			stopSound()
+		}
+	}, [pathname, playSound, stopSound])
+
+	return (
+		<html lang="en">
+			<body>
+				<div className="bg-body">{children}</div>
+			</body>
+		</html>
+	)
+}
