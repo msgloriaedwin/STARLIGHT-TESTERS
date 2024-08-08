42718844dc60f10eae15464cd13eb6b32d53c10e
maiqel <michaelolowe321@gmail.com>
2024-08-08 14:24:12 +0100

    fix: added validation to forgot password

--- app/(authentication)/auth/forgot-password/page.tsx
+++ app/(authentication)/auth/forgot-password/page.tsx
@@ -10,42 +10,50 @@ import ForgotPasswordNavbar from "@/app/components/navbars/custom-navbars/Forgot
 
 
 const ForgotPassword = () => {
-
     const [email, setEmail] = useState('');
-  const router = useRouter();
+    const [error, setError] = useState(false);
+    const router = useRouter();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    localStorage.setItem('resetEmail', email);
-    router.push('/auth/forgot-password/otp');
-  };
-  return (
-    <>
-    <ForgotPasswordNavbar/>
-    <div className="bg-body flex flex-col h-screen justify-center items-center">
-        <div className="mb-6">
-            <p className='text-2xl md:text-4xl mb-3'>Forgot Password?</p>
-            <p className='text-sm md:text-lg'>Enter your Email to receive a code</p>
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!email) {
+            setError(true);
+            return;
+        }
+        localStorage.setItem('resetEmail', email);
+        router.push('/auth/forgot-password/otp');
+    };
+
+    return (
+        <>
+        <ForgotPasswordNavbar/>
+        <div className="bg-body flex flex-col h-screen justify-center items-center">
+            <div className="mb-6">
+                <p className='text-2xl md:text-4xl mb-3'>Forgot Password?</p>
+                <p className='text-sm md:text-lg'>Enter your Email to receive a code</p>
+            </div>
+            <FormCard size='lg'>
+            <form onSubmit={handleSubmit} className='space-y-4'>
+                <RBInput
+                    label='Enter Email'
+                    placeholder='Your Email'
+                    type="email" 
+                    value={email} 
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                        setError(false);
+                    }} 
+                    className={`border p-5 ${error ? 'border-red-500' : 'border-[#C5C5C5]'}`}
+                />
+                <CustomButton size={"lg"} className='w-full p-6 text-base md:text-lg' type='submit'>
+                    Reset password
+                </CustomButton>
+                </form>
+                <Link href={'/auth/login'} className='text-[#00222E] text-base md:text-lg underline flex justify-center mt-10'>Back to sign in</Link>
+            </FormCard>
         </div>
-        <FormCard size='lg'>
-        <form onSubmit={handleSubmit} className='space-y-4'>
-            <RBInput
-              label='Enter Email'
-              placeholder='Your Email'
-              type="email" 
-              value={email} 
-              onChange={(e) => setEmail(e.target.value)} 
-              className='border border-[#C5C5C5] p-5'
-            />
-            <CustomButton size={"lg"} className='w-full p-6 text-base md:text-lg' type='submit'>
-              Reset password
-            </CustomButton>
-          </form>
-            <Link href={'/auth/login'} className='text-[#00222E] text-base md:text-lg underline flex justify-center mt-10'>Back to sign in</Link>
-        </FormCard>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default ForgotPassword
