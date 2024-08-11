db790c28c96bc2b663617d85781cc63797b78e92
Tella Boluwatife <42048129+codeReaper0@users.noreply.github.com>
2024-08-11 07:32:53 +0100

    Update dev_deploy.sh

--- scripts/dev_deploy.sh
+++ scripts/dev_deploy.sh
@@ -5,4 +5,5 @@ set -e
 cd "$(git rev-parse --show-toplevel)"
 git stash
 git pull origin dev
+docker pull hngtechie/bingofe:dev
 docker compose --project-name dev-bingofe -f docker/development/docker-compose.yml up -d
