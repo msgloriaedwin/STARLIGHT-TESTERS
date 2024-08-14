5ec59835534cce236601e5bef3246ee10c9c32a4
Tella Boluwatife <42048129+codeReaper0@users.noreply.github.com>
2024-08-14 12:10:49 +0100

    Update staging_deploy.sh

--- scripts/staging_deploy.sh
+++ scripts/staging_deploy.sh
@@ -9,19 +9,17 @@ export NEXT_PUBLIC_API_SIGN_UP_URL=${NEXT_PUBLIC_API_SIGN_UP_URL}
 export NEXT_PUBLIC_API_URL=${NEXT_PUBLIC_API_URL}
 export GIF_API_KEY=${GIF_API_KEY}
 
+# Navigate to the root of the repository
 cd "$(git rev-parse --show-toplevel)"
 git stash
 git pull origin staging
 docker pull hngtechie/bingofe:staging
 
+# Set the project name for Docker Compose (optional)
+export COMPOSE_PROJECT_NAME=staging-bingofe
 
-# Pass environment variables to Docker Compose
-docker compose --project-name staging-bingofe -f docker/staging/docker-compose.yml up -d \
-  -e NEXT_PUBLIC_API_GOOGLE_SIGN_UP_URL=$NEXT_PUBLIC_API_GOOGLE_SIGN_UP_URL \
-  -e NEXT_PUBLIC_API_SIGN_IN_URL=$NEXT_PUBLIC_API_SIGN_IN_URL \
-  -e NEXT_PUBLIC_API_SIGN_UP_URL=$NEXT_PUBLIC_API_SIGN_UP_URL \
-  -e NEXT_PUBLIC_API_URL=$NEXT_PUBLIC_API_URL \
-  -e GIF_API_KEY=$GIF_API_KEY
+# Bring up the services with environment variables
+docker compose -f docker/staging/docker-compose.yml up -d
 
 
 # set -e
