d846bbc4df4e1c313f5b90889a9fdf6cc091ae6d
Tella Boluwatife <42048129+codeReaper0@users.noreply.github.com>
2024-08-14 18:36:40 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,21 @@
-// ecosystem.config.js
 module.exports = {
   apps: [
     {
       name: 'bingo-staging',
-      script: './node_modules/.bin/next', // Path to the Next.js executable
-      args: 'start -p 6001', // Start Next.js on port 6001
+      script: '.next/standalone/server.js', // Path to the Next.js server.js
+      instances: 1,
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: 'development',
+        PORT: 6001,
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
         GIF_API_KEY: process.env.GIF_API_KEY,
       },
-    },
-  ],
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 6001,
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        GIF_API_KEY: process.env.GIF_API_KEY,
+      }
+    }
+  ]
 };
