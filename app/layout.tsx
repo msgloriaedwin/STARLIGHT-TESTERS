b8b1e02e537b72351e297813840256e64f347d6a
AdekolaToniloba <atoniloba@gmail.com>
2024-08-14 13:12:20 +0100

    Updated the env variables to align with that of the devops production env, wrapped the app layout in the AuthProvider component so that other devs can all it from anywhere in the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import { NextIntlClientProvider } from 'next-intl';
-import { getLocale, getMessages } from 'next-intl/server';
+import { NextIntlClientProvider } from "next-intl";
+import { getLocale, getMessages } from "next-intl/server";
 
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "./providers/QueryClientProvider";
+import { AuthProvider } from "@/context/AuthContext";
 
 const dmSans = DM_Sans({ subsets: ["latin"], weight: ["400", "500", "700"] });
-
 
 export const metadata: Metadata = {
   title: "Bingo",
@@ -19,7 +19,6 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   const locale = await getLocale();
 
   // Providing all messages to the client
@@ -28,12 +27,12 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={`${dmSans.className} bg-primary`}>
-        <NextIntlClientProvider messages={messages}>
-          <QueryProvider>
-            {children}
-          </QueryProvider>
-      </NextIntlClientProvider></body>
-
+        <AuthProvider>
+          <NextIntlClientProvider messages={messages}>
+            <QueryProvider>{children}</QueryProvider>
+          </NextIntlClientProvider>
+        </AuthProvider>
+      </body>
     </html>
   );
 }
