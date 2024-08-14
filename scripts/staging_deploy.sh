f5198c5d8452504cdb63b2eeb794fa174558b7f0
Tella Boluwatife <42048129+codeReaper0@users.noreply.github.com>
2024-08-14 09:19:42 +0100

    Update staging_deploy.sh

--- scripts/staging_deploy.sh
+++ scripts/staging_deploy.sh
@@ -2,8 +2,29 @@
 
 set -e
 
+# Use environment variables
+export NEXT_PUBLIC_API_GOOGLE_SIGN_UP_URL=${NEXT_PUBLIC_API_GOOGLE_SIGN_UP_URL}
+export NEXT_PUBLIC_API_SIGN_IN_URL=${NEXT_PUBLIC_API_SIGN_IN_URL}
+export NEXT_PUBLIC_API_SIGN_UP_URL=${NEXT_PUBLIC_API_SIGN_UP_URL}
+export NEXT_PUBLIC_API_URL=${NEXT_PUBLIC_API_URL}
+
 cd "$(git rev-parse --show-toplevel)"
 git stash
 git pull origin staging
 docker pull hngtechie/bingofe:staging
-docker compose --project-name staging-bingofe -f docker/staging/docker-compose.yml up -d
+
+# Pass environment variables to Docker Compose
+docker compose --project-name staging-bingofe -f docker/staging/docker-compose.yml up -d \
+  -e NEXT_PUBLIC_API_GOOGLE_SIGN_UP_URL=$NEXT_PUBLIC_API_GOOGLE_SIGN_UP_URL \
+  -e NEXT_PUBLIC_API_SIGN_IN_URL=$NEXT_PUBLIC_API_SIGN_IN_URL \
+  -e NEXT_PUBLIC_API_SIGN_UP_URL=$NEXT_PUBLIC_API_SIGN_UP_URL \
+  -e NEXT_PUBLIC_API_URL=$NEXT_PUBLIC_API_URL
+
+
+# set -e
+
+# cd "$(git rev-parse --show-toplevel)"
+# git stash
+# git pull origin staging
+# docker pull hngtechie/bingofe:staging
+# docker compose --project-name staging-bingofe -f docker/staging/docker-compose.yml up -d
