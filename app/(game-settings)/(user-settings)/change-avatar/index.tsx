ef3a44a1bdf9aa6f6eb496ab0e73b675371c110c
The Hanifah <hanifah2015@gmail.com>
2024-08-14 13:40:51 +0100

    feat/continuation: multiple language integration

--- app/(game-settings)/(user-settings)/change-avatar/index.tsx
+++ app/(game-settings)/(user-settings)/change-avatar/index.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import Image from "Next/image";
 import CustomUploadContainer from "./upload";
 import Modal from "@/app/components/modal/modal";
 
