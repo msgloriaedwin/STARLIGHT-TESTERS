43f098c5df9ba159fbf99078923475966c03d5c8
Tella Boluwatife <42048129+codeReaper0@users.noreply.github.com>
2024-08-14 19:14:22 +0100

    Update staging_deploy.sh

--- scripts/staging_deploy.sh
+++ scripts/staging_deploy.sh
@@ -5,6 +5,7 @@ set -e
 # Assign environment variables
 export NEXT_PUBLIC_API_URL=${NEXT_PUBLIC_API_URL}
 export GIF_API_KEY=${GIF_API_KEY}
+export PORT=6001
 
 # Navigate to the project directory
 cd /home/starlight-nestjs/remote-bingo/staging/bingo_fe
@@ -24,5 +25,17 @@ rm -rf .next
 # Build the application
 pnpm run build
 
-# Start or restart the application using PM2
-pm2 start ecosystem.config.js --env production
+# Define the PM2 application name
+APP_NAME="nextjs-app"
+
+# Check if the application is already running and restart it
+if pm2 list | grep -q $APP_NAME; then
+  echo "Application is already running. Restarting..."
+  pm2 restart $APP_NAME
+else
+  echo "Application is not running. Starting..."
+  pm2 start ecosystem.config.js --name $APP_NAME --env production
+fi
+
+# Save the PM2 process list and environment
+pm2 save
