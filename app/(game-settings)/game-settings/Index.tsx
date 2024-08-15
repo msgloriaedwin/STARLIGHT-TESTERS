20fa342ca88a56a926782a76178bd2d06b3375df
maiqel <michaelolowe321@gmail.com>
2024-08-15 13:57:43 +0100

    feat: integrated game settings with backend

--- app/(game-settings)/game-settings/Index.tsx
+++ app/(game-settings)/game-settings/Index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 import { ToggleControlSettings } from "@/app/components/ToggleControl/ToggleControlSettings";
@@ -29,7 +29,7 @@ const Index: React.FC = () => {
   const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
   useEffect(() => {
-    const storedSettings = localStorage.getItem('gameSettings');
+    const storedSettings = localStorage.getItem("gameSettings");
     if (storedSettings) {
       setSettings(JSON.parse(storedSettings));
     }
@@ -50,7 +50,7 @@ const Index: React.FC = () => {
       });
       const fetchedSettings = response.data.data.game_settings;
       setSettings(fetchedSettings);
-      localStorage.setItem('gameSettings', JSON.stringify(fetchedSettings));
+      localStorage.setItem("gameSettings", JSON.stringify(fetchedSettings));
     } catch (error) {
       console.error("Failed to fetch game settings:", error);
     } finally {
@@ -61,76 +61,75 @@ const Index: React.FC = () => {
   const handleSettingChange = (key: keyof GameSettings, value: boolean) => {
     const newSettings = { ...settings, [key]: value };
     setSettings(newSettings);
-    localStorage.setItem('gameSettings', JSON.stringify(newSettings));
+    localStorage.setItem("gameSettings", JSON.stringify(newSettings));
   };
 
   const handleFormSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-  
+
     // Only send the necessary fields to the backend
     const payload = {
       is_notification: settings.is_notification,
       is_sound: settings.is_sound,
     };
-  
+
     try {
-      await axios.patch(
-        `${baseUrl}game-settings/me`,
-        payload,
-        {
-          headers: {
-            Authorization: `Bearer ${user.access_token}`,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+      await axios.patch(`${baseUrl}game-settings/me`, payload, {
+        headers: {
+          Authorization: `Bearer ${user.access_token}`,
+          "Content-Type": "application/json",
+        },
+      });
       setIsModalOpen(true);
     } catch (error) {
       console.error("Failed to update settings:", error);
     }
   };
-  
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="px-6 pt-16 bg-[#F7EEE7]">
-      <div className="max-w-[411px]">
-        <h1 className="text-4xl text-primary-700">{t("title")}</h1>
+    <div className='px-6 pt-16 bg-[#F7EEE7]'>
+      <div className='max-w-[411px]'>
+        <h1 className='text-4xl text-primary-700'>{t("title")}</h1>
 
-        <form onSubmit={handleFormSubmit} className="mt-16">
-          <div className="flex items-center justify-between">
-            <p className="text-xl font-medium text-primary-700">
+        <form onSubmit={handleFormSubmit} className='mt-16'>
+          <div className='flex items-center justify-between'>
+            <p className='text-xl font-medium text-primary-700'>
               {t("notificationsLabel")}
             </p>
             <ToggleControlSettings
-              className="data-[state=checked]:bg-primary-200 h-9"
-              thumbClassName="data-[state=checked]:bg-[#fff] h-8"
+              className='data-[state=checked]:bg-primary-200 h-9'
+              thumbClassName='data-[state=checked]:bg-[#fff] h-8'
               checked={settings.is_notification}
-              onCheckedChange={(checked) => handleSettingChange("is_notification", checked)}
+              onCheckedChange={(checked) =>
+                handleSettingChange("is_notification", checked)
+              }
             />
           </div>
 
-          <div className="flex items-center justify-between my-6">
-            <p className="text-xl font-medium text-primary-700">
+          <div className='flex items-center justify-between my-6'>
+            <p className='text-xl font-medium text-primary-700'>
               {t("soundEffectsLabel")}
             </p>
             <ToggleControlSettings
-              className="data-[state=checked]:bg-primary-200 h-9"
-              thumbClassName="data-[state=checked]:bg-[#fff] h-8"
+              className='data-[state=checked]:bg-primary-200 h-9'
+              thumbClassName='data-[state=checked]:bg-[#fff] h-8'
               checked={settings.is_sound}
-              onCheckedChange={(checked) => handleSettingChange("is_sound", checked)}
+              onCheckedChange={(checked) =>
+                handleSettingChange("is_sound", checked)
+              }
             />
           </div>
 
           <CustomButton
-            type="submit"
-            variant="default"
-            size="lg"
-            className="w-36 h-14 mt-12 text-base font-normal text-primary-B900"          
-            ariaLabel="Save changes"
+            type='submit'
+            variant='default'
+            size='lg'
+            className='w-36 h-14 mt-12 text-base font-normal text-primary-B900'
+            ariaLabel='Save changes'
           >
             {t("saveChangesButton")}
           </CustomButton>
